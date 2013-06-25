eb515d8e6011c79d07cb886c4543febe246e88ff
Goldgram <willbrennandesign@gmail.com>
2013-06-25 01:14:26 +0100

    completed 9 and started 10

--- 09.php
+++ 09.php
@@ -1,13 +1,25 @@
 <!-- 
-
+A Pythagorean triplet is a set of three natural numbers, a  b  c, for which,
+a2 + b2 = c2
+For example, 32 + 42 = 9 + 16 = 25 = 52.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
  -->
 <?php $startTime = microtime(true);
 
+$triplet;
+for ($a=1; $a <= 998; $a++) { 
+	for ($b=1; $b <= (999-$a); $b++) { 
+		for ($c=1; $c <= (1000-$a-$b); $c++) { 
+			if ($a+$b+$c==1000 && ($a*$a)+($b*$b)==($c*$c)) {
+				$triplet = $a*$b*$c;
+				break 3;
+			}
+		}
+	}
+}
 
-
-
-
-$answer = 0;
+$answer = $triplet;
 $endTime = microtime(true);
 echo 'Answer: ',$answer,'<br>Time: ',($endTime - $startTime);
 ?>
--- 10.php
+++ 10.php
@@ -1,24 +1,51 @@
 <?php $startTime = microtime(true);
 
-$numbers = array();
-for ($i=2; $i <= 10; $i++) { 
-	$numbers[$i] = $i;
-}
-
-for ($i=2; $i <= 10; $i++) { 
-	//echo $i,'<br>';
-	foreach ($numbers as $key => $value) {
-		if ($value%$i==0 && $value!=$i) {
-			unset($numbers[$key]);
+function isPrime($input,$inputArray)
+{
+	$bigCheck = ''+($input);
+	$coin = 1;
+	for ($i=0; $i < count($inputArray); $i++) {
+		$check = ''+$inputArray[$i];
+		if (bcmod($bigCheck, $check)==0) {
+			$coin--;
+			break;
 		}
-		// echo $key,' => ',$value,'<br>';
 	}
+	return $coin;
 }
-echo array_sum($numbers)
+
+// $numbers = array();
+// for ($i=2; $i <= 10; $i++) { 
+// 	$numbers[$i] = $i;
+// }
+
+// for ($i=2; $i <= 10; $i++) { 
+// 	//echo $i,'<br>';
+// 	foreach ($numbers as $key => $value) {
+// 		if ($value%$i==0 && $value!=$i) {
+// 			unset($numbers[$key]);
+// 		}
+// 		// echo $key,' => ',$value,'<br>';
+// 	}
+// }
+// echo array_sum($numbers),'<br>';
+$totalPrimes = 0;
+$primeArray = array();
+$primeArray[0] = 2;
+$primeArrayIndex = 1;
+for ($i=3; $i <= 4000000; $i++) {
+	if (isPrime($i,$primeArray)==1)
+	{
+		$primeArray[$primeArrayIndex] = $i;
+		$primeArrayIndex++;
+		$totalPrimes += $i; 
+	}
+	var_dump($primeArray);
+	echo '<br><br>';
+}
 
 
-
-$answer = 'not finished';
+$answer = $totalPrimes;
 $endTime = microtime(true);
 echo 'Answer: ',$answer,'<br>Time: ',($endTime - $startTime);
 ?>
