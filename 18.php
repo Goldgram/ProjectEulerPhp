22296522cf2fdf79d15d919a8646d45731adb6fc
Goldgram <willbrennandesign@gmail.com>
2013-06-30 16:45:07 +0100

    Solved 67 using same logic as 18

--- 18.php
+++ 18.php
@@ -24,33 +24,25 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
  -->
 <?php $startTime = microtime(true);
-//$tri = array();
-// $arrayName = array('' => , );
-// $tri[0] = array(3);
-// $tri[1] = array(7,4);
-// $tri[2] = array(2,4,6);
-// $tri[3] = array(8,5,9,3);
 
+//needed to take out any leading 0 for number under 10
 $tri[0] = array(75);
 $tri[1] = array(95,64);
 $tri[2] = array(17,47,82);
 $tri[3] = array(18,35,87,10);
-$tri[4] = array(20,04,82,47,65);
-$tri[5] = array(19,01,23,75,03,34);
-$tri[6] = array(88,02,77,73,07,63,67);
-$tri[7] = array(99,65,04,28,06,16,70,92);
+$tri[4] = array(20,4,82,47,65);
+$tri[5] = array(19,1,23,75,3,34);
+$tri[6] = array(88,2,77,73,7,63,67);
+$tri[7] = array(99,65,4,28,6,16,70,92);
 $tri[8] = array(41,41,26,56,83,40,80,70,33);
 $tri[9] = array(41,48,72,33,47,32,37,16,94,29);
 $tri[10] = array(53,71,44,65,25,43,91,52,97,51,14);
 $tri[11] = array(70,11,33,28,77,73,17,78,39,68,17,57);
 $tri[12] = array(91,71,52,38,17,14,91,43,58,50,27,29,48);
-$tri[13] = array(63,66,04,68,89,53,67,30,73,16,69,87,40,31);
-$tri[14] = array(04,62,98,27,23,09,70,98,73,93,38,53,60,04,23);
+$tri[13] = array(63,66,4,68,89,53,67,30,73,16,69,87,40,31);
+$tri[14] = array(04,62,98,27,23,9,70,98,73,93,38,53,60,4,23);
 
 $sumTri[0] = array(75);
-// $sumTri[1] = array(0,0);
-// $sumTri[2] = array(0,0,0);
-// $sumTri[3] = array(0,0,0,0);
 
 for ($i=1; $i <= 14; $i++) {
 	for ($j=0; $j < ($i+1); $j++) { 
@@ -60,36 +52,16 @@ for ($i=1; $i <= 14; $i++) {
 
 for ($i=0; $i < 14; $i++) {
 	for ($j=0; $j < count($tri[$i]); $j++) { 
-		//echo $tri[$i][$j]," => ",$sumTri[$i][$j],"\n";
-
-		//$sumTri[$i+1][$j] = $sumTri[$i][$j] + $tri[$i+1][$j];
 		$tempsum1 = $sumTri[$i][$j] + $tri[$i+1][$j];
 		if ($tempsum1>$sumTri[$i+1][$j]) {
 			$sumTri[$i+1][$j] = $tempsum1;
 		}
-
-
-		//$sumTri[$i+1][$j+1] = $sumTri[$i][$j] + $tri[$i+1][$j+1];
 		$tempsum2 = $sumTri[$i][$j] + $tri[$i+1][$j+1];
 		if ($tempsum2>$sumTri[$i+1][$j+1]) {
 			$sumTri[$i+1][$j+1] = $tempsum2;
 		}
 	}
 }
-
-// for ($i=0; $i < 15; $i++) {
-// 	for ($j=0; $j < count($tri[$i]); $j++) { 
-// 		echo $tri[$i][$j]," ";
-// 	}
-// 	echo "\n";
-// }
-// echo "---\n";
-// for ($i=0; $i < 15; $i++) {
-// 	for ($j=0; $j < count($tri[$i]); $j++) { 
-// 		echo $sumTri[$i][$j]," ";
-// 	}
-// 	echo "\n";
-// }
 $maxSum = 0;
 for ($i=0; $i < count($sumTri[14]); $i++) { 
 	$sumTri[14][$i];
@@ -97,8 +69,6 @@ for ($i=0; $i < count($sumTri[14]); $i++) {
 		$maxSum = $sumTri[14][$i];
 	}
 }
-
-
 
 $answer = $maxSum;
 $endTime = microtime(true);
