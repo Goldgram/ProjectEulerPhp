ba49b07c582502695d98a391ef04a992108746e4
Goldgram <willbrennandesign@gmail.com>
2013-06-30 15:41:22 +0100

    complete 18, code needs clean up

--- 18.php
+++ 18.php
@@ -24,19 +24,41 @@ Find the maximum total from top to bottom of the triangle below:
 NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
  -->
 <?php $startTime = microtime(true);
-$tri = array();
+//$tri = array();
 // $arrayName = array('' => , );
-$tri[0] = array(3);
-$tri[1] = array(7,4);
-$tri[2] = array(2,4,6);
-$tri[3] = array(8,5,9,3);
+// $tri[0] = array(3);
+// $tri[1] = array(7,4);
+// $tri[2] = array(2,4,6);
+// $tri[3] = array(8,5,9,3);
 
-$sumTri[0] = array(3);
-$sumTri[1] = array(0,0);
-$sumTri[2] = array(0,0,0);
-$sumTri[3] = array(0,0,0,0);
+$tri[0] = array(75);
+$tri[1] = array(95,64);
+$tri[2] = array(17,47,82);
+$tri[3] = array(18,35,87,10);
+$tri[4] = array(20,04,82,47,65);
+$tri[5] = array(19,01,23,75,03,34);
+$tri[6] = array(88,02,77,73,07,63,67);
+$tri[7] = array(99,65,04,28,06,16,70,92);
+$tri[8] = array(41,41,26,56,83,40,80,70,33);
+$tri[9] = array(41,48,72,33,47,32,37,16,94,29);
+$tri[10] = array(53,71,44,65,25,43,91,52,97,51,14);
+$tri[11] = array(70,11,33,28,77,73,17,78,39,68,17,57);
+$tri[12] = array(91,71,52,38,17,14,91,43,58,50,27,29,48);
+$tri[13] = array(63,66,04,68,89,53,67,30,73,16,69,87,40,31);
+$tri[14] = array(04,62,98,27,23,09,70,98,73,93,38,53,60,04,23);
 
-for ($i=0; $i < 3; $i++) {
+$sumTri[0] = array(75);
+// $sumTri[1] = array(0,0);
+// $sumTri[2] = array(0,0,0);
+// $sumTri[3] = array(0,0,0,0);
+
+for ($i=1; $i <= 14; $i++) {
+	for ($j=0; $j < ($i+1); $j++) { 
+		$sumTri[$i][$j] = 0;
+	}
+}
+
+for ($i=0; $i < 14; $i++) {
 	for ($j=0; $j < count($tri[$i]); $j++) { 
 		//echo $tri[$i][$j]," => ",$sumTri[$i][$j],"\n";
 
@@ -53,27 +75,26 @@ for ($i=0; $i < 3; $i++) {
 			$sumTri[$i+1][$j+1] = $tempsum2;
 		}
 	}
-	//echo "--\n";
 }
 
-// for ($i=0; $i < 4; $i++) {
+// for ($i=0; $i < 15; $i++) {
 // 	for ($j=0; $j < count($tri[$i]); $j++) { 
 // 		echo $tri[$i][$j]," ";
 // 	}
 // 	echo "\n";
 // }
 // echo "---\n";
-// for ($i=0; $i < 4; $i++) {
+// for ($i=0; $i < 15; $i++) {
 // 	for ($j=0; $j < count($tri[$i]); $j++) { 
 // 		echo $sumTri[$i][$j]," ";
 // 	}
 // 	echo "\n";
 // }
 $maxSum = 0;
-for ($i=0; $i < count($sumTri[3]); $i++) { 
-	$sumTri[3][$i];
-	if ($sumTri[3][$i]>$maxSum) {
-		$maxSum = $sumTri[3][$i];
+for ($i=0; $i < count($sumTri[14]); $i++) { 
+	$sumTri[14][$i];
+	if ($sumTri[14][$i]>$maxSum) {
+		$maxSum = $sumTri[14][$i];
 	}
 }
 
