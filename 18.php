ad806e22fa6df419c19720124825097d68ed2d16
Goldgram <willbrennandesign@gmail.com>
2013-06-30 15:30:47 +0100

    working on 18, working for small triangle

--- 18.php
+++ 18.php
@@ -1,10 +1,84 @@
 <!-- 
-
+By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+3
+7 4
+2 4 6
+8 5 9 3
+That is, 3 + 7 + 4 + 9 = 23.
+Find the maximum total from top to bottom of the triangle below:
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
  -->
 <?php $startTime = microtime(true);
+$tri = array();
+// $arrayName = array('' => , );
+$tri[0] = array(3);
+$tri[1] = array(7,4);
+$tri[2] = array(2,4,6);
+$tri[3] = array(8,5,9,3);
+
+$sumTri[0] = array(3);
+$sumTri[1] = array(0,0);
+$sumTri[2] = array(0,0,0);
+$sumTri[3] = array(0,0,0,0);
+
+for ($i=0; $i < 3; $i++) {
+	for ($j=0; $j < count($tri[$i]); $j++) { 
+		//echo $tri[$i][$j]," => ",$sumTri[$i][$j],"\n";
+
+		//$sumTri[$i+1][$j] = $sumTri[$i][$j] + $tri[$i+1][$j];
+		$tempsum1 = $sumTri[$i][$j] + $tri[$i+1][$j];
+		if ($tempsum1>$sumTri[$i+1][$j]) {
+			$sumTri[$i+1][$j] = $tempsum1;
+		}
+
+
+		//$sumTri[$i+1][$j+1] = $sumTri[$i][$j] + $tri[$i+1][$j+1];
+		$tempsum2 = $sumTri[$i][$j] + $tri[$i+1][$j+1];
+		if ($tempsum2>$sumTri[$i+1][$j+1]) {
+			$sumTri[$i+1][$j+1] = $tempsum2;
+		}
+	}
+	//echo "--\n";
+}
+
+// for ($i=0; $i < 4; $i++) {
+// 	for ($j=0; $j < count($tri[$i]); $j++) { 
+// 		echo $tri[$i][$j]," ";
+// 	}
+// 	echo "\n";
+// }
+// echo "---\n";
+// for ($i=0; $i < 4; $i++) {
+// 	for ($j=0; $j < count($tri[$i]); $j++) { 
+// 		echo $sumTri[$i][$j]," ";
+// 	}
+// 	echo "\n";
+// }
+$maxSum = 0;
+for ($i=0; $i < count($sumTri[3]); $i++) { 
+	$sumTri[3][$i];
+	if ($sumTri[3][$i]>$maxSum) {
+		$maxSum = $sumTri[3][$i];
+	}
+}
 
 
 
-$answer = 0;
+$answer = $maxSum;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
