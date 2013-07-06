7742df1cfd5cb4bb3a743592e3d72b0a958b5964
Goldgram <willbrennandesign@gmail.com>
2013-07-06 21:37:48 +0100

    Complete but needs cleaning up

--- 24.php
+++ 24.php
@@ -9,13 +9,13 @@ $nums = array();
 $nums[0] = "0";
 $nums[1] = "1";
 $nums[2] = "2";
-// $nums[3] = "3";
-// $nums[4] = "4";
-// $nums[5] = "5";
-// $nums[6] = "6";
-// $nums[7] = "7";
-// $nums[8] = "8";
-// $nums[9] = "9";
+$nums[3] = "3";
+$nums[4] = "4";
+$nums[5] = "5";
+$nums[6] = "6";
+$nums[7] = "7";
+$nums[8] = "8";
+$nums[9] = "9";
 
 $result = array();
 
@@ -73,7 +73,7 @@ function printResult()
 }
 
 
-$perNum = 6;
+$perNum = 1000000;
 //for ($perNum=1; $perNum <= 6; $perNum++) { 
 	for ($numIndex=0; $numIndex < count($nums); $numIndex++) { 
 		//$numIndex = 1;
