e29f1c1a0357edcb80eb93ae64f3c0be759db956
Will Brennan <willbrennan@Wills-iMac.local>
2013-07-08 08:51:33 +0100

    completely refactored the search for reoccurring values, now can execute in less than a second

--- 26.php
+++ 26.php
@@ -16,48 +16,26 @@ Find the value of d  1000 for which 1/d contains the longest recurring cycle in 
 
 function isMulRec($input)//is recursive of multiple nums
 {
-$numerator = 1;
-$numText = "0.";
-$leftover = 1;
-//for ($i=0; $i < 10000; $i++) {
-while ($leftover>0) {
-	$number = floor(($numerator*10)/$input);
-	$leftover = ($numerator*10)%$input;
-	//echo $number," ~ ",$leftover;
-	$numText .= $number;
-
-	if (strpos($numText, $number.$number.$number.$number)) {
-		//echo "<=\n";
-		return 1;
-	}
-
-
-	//echo "\n";
-
-	$inputStr = substr($numText,(strlen("".$input)+1));
-	for ($i=0; $i < strlen($inputStr); $i+=2) { 
-		$inputStr = substr($inputStr,1,strlen($inputStr)-2);
-		if (strlen($inputStr)%2==0 && strlen($inputStr)>2) {
-			//$inputStr = "".$input;
-			//echo substr($numText,(strlen($inputStr)+1)),"\n";
-			// echo $inputStr,"\n";
-			// echo substr($inputStr,0,strlen($inputStr)/2)," - ",substr($inputStr,strlen($inputStr)/2),"\n";
-			$firstHalf = substr($inputStr,0,strlen($inputStr)/2);
-			$firstHalfLength = strlen($firstHalf);
-			$secondHalf = substr($inputStr,strlen($inputStr)/2);
-			if ($firstHalf==$secondHalf) {
-				// echo "> ",$firstHalfLength," <\n";
-				return $firstHalfLength;
-
-			}
+	$numerator = 1;
+	$numText = "0.";
+	$leftover = 1;
+	while ($leftover>0) {
+		$number = floor(($numerator*10)/$input);
+		$leftover = ($numerator*10)%$input;
+		$currentText = ".".$number."-".$leftover.".";
+		$numText .= $currentText;
+		$testText = "";
+		for ($i=0; $i < 4; $i++) { 
+			$testText .= $currentText;
 		}
+		if (substr_count($numText, $currentText)>1) {
+			// this isn't the exact number due to adding "." and "-" in but the offset is exact across all numbers, it's roughly x5 plus leading values that don't reoccur
+			//this returns 5 for any single digit cycle
+			return strripos($numText, $currentText)-stripos($numText, $currentText);
+		}
+		$numerator = $leftover;
 	}
-
-	$numerator = $leftover;
-}
-return 0;
-
-	//return 0
+	return 0;
 }
 $HighestVal = 0;
 $HighestNum = 0;
@@ -68,8 +46,8 @@ for ($i=2; $i < 1000; $i++) {
 		$HighestVal = $iRecValue;
 		$HighestNum = $i;
 	}
-	// echo $i," => ",$iRecValue,"\n";
-	// echo "===================================================\n";
+	//echo $i," => ",$iRecValue,"\n";
+	//echo "===================================================\n";
 }
 
 
