8d403d404ba4652cb94b65d13a23ab01f1d5e682
Goldgram <willbrennandesign@gmail.com>
2013-07-10 10:01:35 +0100

    Sudo code ish answer

--- 28.php
+++ 28.php
@@ -2,8 +2,18 @@
 
  -->
 <?php $startTime = microtime(true);
+$total=1;
+$int =2;
+$num=1;
+While $num<1001*1001
 
+For 4 times
+$num += $int;
+$total = $num;
 
+$int+=2;
+
+Echo total
 
 $answer = 0;
 $endTime = microtime(true);
