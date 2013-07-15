07fb1632471b403988ec5800ec3c96f63183bf72
Will Brennan <willbrennan@Wills-iMac.local>
2013-07-15 17:53:27 +0100

    Complete 36 in 0.32s

--- 36.php
+++ 36.php
@@ -1,12 +1,29 @@
 <!-- 
-
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+(Please note that the palindromic number, in either base, may not include leading zeros.)
  -->
 <?php $startTime = microtime(true);
 
+function isPalindromic($input)
+{
+	$inputStr = "".$input;
+	if ($inputStr == strrev($inputStr)){
+		return true;
+	}
+	return false;
+}
+$total = 0;
+for ($num=1; $num < 1000000; $num+=2) 
+{ 
+	if (isPalindromic($num) && isPalindromic(decbin($num))) 	
+	{
+		$total += $num;
+	}
+}
 
-
-$answer = 0;
+$answer = $total;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
-// Answer: 
-// Time: 
+// Answer: 872187
+// Time: 0.32s
