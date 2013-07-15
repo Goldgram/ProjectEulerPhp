3babfb11255177e1971a11464f4d773f572b3cf0
Will Brennan <willbrennan@Wills-iMac.local>
2013-07-15 17:54:09 +0100

    Working on 39

--- 39.php
+++ 39.php
@@ -1,8 +1,26 @@
 <!-- 
-
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+{20,48,52}, {24,45,51}, {30,40,50}
+For which value of p ≤ 1000, is the number of solutions maximised?
  -->
 <?php $startTime = microtime(true);
 
+for ($p=6; $p <= 100; $p++) { 
+	$count = 0;
+	for ($a=2; $a <= ($p/2); $a++) {
+		for ($b=$a; $b <= ($p/2); $b++) {
+			$c = $p - $a - $b;
+			if (($a*$a)+($b*$b) == ($c*$c))
+			{
+				// echo $a," , ",$b," , ",$c,"\n";
+				$count++;
+			}
+		}
+	}
+	if ($count>0) {
+		echo $p," => ",$count,"\n";
+	}
+}
 
 
 $answer = 0;
