e7b21072e157392dd96e505c05db800a8b97a4fe
Goldgram <willbrennandesign@gmail.com>
2013-07-15 22:26:59 +0100

    Started working on 41

--- 41.php
+++ 41.php
@@ -1,7 +1,18 @@
 <!-- 
-
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+What is the largest n-digit pandigital prime that exists?
  -->
 <?php $startTime = microtime(true);
+
+function isPrime($input) {
+	$sq = sqrt($input);
+	for ($i=2; $i <= $sq; $i++) {
+		if ($input%$i==0) {
+			return false;
+		}
+	}
+	return true;
+}
 
 
 
