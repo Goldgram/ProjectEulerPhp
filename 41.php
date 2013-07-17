aa24920dee5501f792d8d1bc77bd4a6a86b48e14
Goldgram <willbrennandesign@gmail.com>
2013-07-17 07:30:52 +0100

    refactored, executes in 4.8s

--- 41.php
+++ 41.php
@@ -13,125 +13,32 @@ function isPrime($input) {
 	}
 	return true;
 }
-
-// $count = 0;
-// for($a=9; $a >= 1; $a--) {//1
-// 	for($b=9; $b >= 1; $b--) {//2
-// 		if($b==$a){continue;}
-// 		for($c=9; $c >= 1; $c--) {//3
-// 			if($c==$a||$c==$b){continue;}
-// 			for($d=9; $d >= 1; $d--) {//4
-// 				if($d==$a||$d==$b||$d==$c){continue;}
-// 				for($e=9; $e >= 1; $e--) {//5
-// 					if($e==$a||$e==$b||$e==$c||$e==$d){continue;}
-// 					for($f=9; $f >= 1; $f--) {//6
-// 						if($f==$a||$f==$b||$f==$c||$f==$d||$f==$e){continue;}
-// 						for($g=9; $g >= 1; $g--) {//7
-// 							if($g==$a||$g==$b||$g==$c||$g==$d||$g==$e||$g==$f){continue;}
-// 							for($h=9; $h >= 1; $h--) {//8
-// 								if($h==$a||$h==$b||$h==$c||$h==$d||$h==$e||$h==$f||$h==$g){continue;}
-// 								for($i=9; $i >= 1; $i--) {//9
-// 									if($i==$a||$i==$b||$i==$c||$i==$d||$i==$e||$i==$f||$i==$g||$i==$h){continue;}
-// 									// $count++;
-// 									$testStr = "".$a.$b.$c.$d.$e.$f.$g.$h.$i;
-// 									if (isPrime(intval($testStr))) {
-// 										echo $testStr,"\n";
-// 									}
-// 								}
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-// echo $count,"\n";
-
-
-
-
-// for($a=9; $a >= 1; $a--) {//1
-// 	$str .= $a;
-// 	for($b=9; $b >= 1; $b--) {//2
-// 		if(strpos($str,"".$b)){continue;}
-// 		$str .= $b;
-// 		for($c=9; $c >= 1; $c--) {//3
-// 			if(strpos($str,"".$c)){continue;}
-// 			$str .= $c;
-// 			for($d=9; $d >= 1; $d--) {//4
-// 				if(strpos($str,"".$d)){continue;}
-// 				$str .= $d;
-// 				for($e=9; $e >= 1; $e--) {//5
-// 					if(strpos($str,"".$e)){continue;}
-// 					$str .= $e;
-// 					for($f=9; $f >= 1; $f--) {//6
-// 						if(strpos($str,"".$f)){continue;}
-// 						$str .= $f;
-// 						for($g=9; $g >= 1; $g--) {//7
-// 							if(strpos($str,"".$g)){continue;}
-// 							$str .= $g;
-// 							for($h=9; $h >= 1; $h--) {//8
-// 								if(strpos($str,"".$h)){continue;}
-// 								$str .= $h;
-// 								for($i=9; $i >= 1; $i--) {//9
-// 									if(strpos($str,"".$i)){continue;}
-// 									// echo $str,"\n";
-// 									// break 10;
-// 									$count++;
-// 									// $testStr = "".$a.$b.$c.$d.$e.$f.$g.$h.$i;
-// 									// if (isPrime(intval($testStr))) {
-// 									// 	echo $testStr,"\n";
-// 									// }
-// 								}
-// 								$str = substr($str,0,-1);
-// 							}
-// 							$str = substr($str,0,-1);
-// 						}
-// 						$str = substr($str,0,-1);
-// 					}
-// 					$str = substr($str,0,-1);
-// 				}
-// 				$str = substr($str,0,-1);
-// 			}
-// 			$str = substr($str,0,-1);
-// 		}
-// 		$str = substr($str,0,-1);
-// 	}
-// 	$str = substr($str,0,-1);
-// }
-// echo $count,"\n";
-
-
-function recLoop($str,$upTo)
+function recLoop($currentStr,$upTo)
 {
-	global $numLoops,$count,$highestVal;
-	$numLoops++;
+	global $highestVal;
 	for($b=$upTo; $b >= 1; $b--) {//2
-		if(strpos($str,"".$b))
+		if(strpos($currentStr,"".$b))
 		{
 			continue;
 		}
-		$str .= $b;
-		if (strlen($str)<=$upTo) {
-			recLoop($str,$upTo);
+		$currentStr .= $b;
+		if (strlen($currentStr)<=$upTo) {
+			recLoop($currentStr,$upTo);
 		}
 		else {
-			$currentNum = intval(substr($str, 1));
+			$currentNum = intval(substr($currentStr, 1));
 			if (isPrime($currentNum)) {
 				if ($currentNum>$highestVal) {
 					$highestVal = $currentNum;
 				}
 			}
 		}
-		$str = substr($str,0,-1);
+		$currentStr = substr($currentStr,0,-1);
 	}
 }
 $highestVal = 0;
 for ($upTo=9; $upTo >= 1; $upTo--) { 
-	$str = ".";
-	$numLoops = 0;
-	recLoop($str,$upTo);
+	recLoop(".",$upTo);
 	if ($highestVal>0) {
 		break;
 	}
@@ -141,4 +48,4 @@ $answer = $highestVal;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
 // Answer: 7652413 
-// Time: 5.4s
+// Time: 4.8s
