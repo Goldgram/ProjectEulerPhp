6947ab2e3cf51bf02ec6f9fcd5d6b554410e5f9b
Goldgram <willbrennandesign@gmail.com>
2013-07-18 08:21:51 +0100

    Sudo code ish start

--- 44.php
+++ 44.php
@@ -3,6 +3,14 @@
  -->
 <?php $startTime = microtime(true);
 
+For $n 1-1000
+$pent[$n] = ($n*((3*$n)-1))/2;
+
+For $i 1-1000
+For $j 1-1000
+If (in_array($pent,$pent[$i]+$pent[$j]))
+
+
 
 
 $answer = 0;
