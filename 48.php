6c5fe643ab46d5d54d82c2a26ebcd82dd8ff46dc
Goldgram <willbrennandesign@gmail.com>
2013-07-23 18:01:51 +0200

    currently placing all permutations of any 4 digit number that has 3 or more primes into an array of arrays, currently has 29 results

--- 48.php
+++ 48.php
@@ -4,38 +4,16 @@ Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
  -->
 <?php $startTime = microtime(true);
 
-$sumString = "0000000000";
-// for ($i=0; $i < 10; $i++) { 
-// 	$sumArray[$i] = 0;
-// }
-
-for ($i=0; $i < 10; $i++) {
-	$numStr = "".pow($i+1,$i+1);
-	$numStrLen = strlen($numStr);
-	// $leftOver = 0;
-	// for ($j=0; $j < 10; $j++) { 
-	// 	$sumArray[$i]
-	// }
-
-	$leftover = 0;
-	for ($j=0; $j < $numStrLen; $j++) { 
-		$current = intval($numStr[($numStrLen-$j-1)]) + intval($sumString[9-$j])+$leftover;
-		$leftover = 0;
-		if ($current>9) {
-			$current-=10;
-			$leftover=1;
-		}
-		$sumString[9-$j] = $current;
-		if ($j==9) {
-			break;
-		}
-	}
-	// $sumArray[$i] = 0;
+$total = "0";
+for ($i=1; $i <= 1000; $i++) {
+	$currentI = "".$i;
+	$currnetNum = bcpow($currentI,$currentI);
+	$total = bcadd($total,$currnetNum);
 }
-var_dump($sumString);	
-
-$answer = 0;
+$lastTenDigits = substr($total,strlen($total)-10);
+ 
+$answer = $lastTenDigits;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
-// Answer: 
-// Time: 
+// Answer: 9110846700
+// Time: 1.95s
