fa984a46ffc6ec0697370aad3250b945693a1998
Goldgram <willbrennandesign@gmail.com>
2013-07-23 18:07:53 +0200

    completed runs in 1.95s

--- 48.php
+++ 48.php
@@ -1,7 +1,7 @@
 <!-- 
 The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
 Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
- -->
+-->
 <?php $startTime = microtime(true);
 
 $total = "0";
