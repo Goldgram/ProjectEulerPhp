e10b70a79d2ae0b7fc635e688bbdcd709551f593
Goldgram <willbrennandesign@gmail.com>
2013-07-26 12:07:17 +0200

    Added templates 51-55

---
+++ 51.php
@@ -0,0 +1,12 @@
+<!-- 
+
+-->
+<?php $startTime = microtime(true);
+
+
+
+$answer = $closestSum;
+$endTime = microtime(true);
+echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
+// Answer: 
+// Time: 
