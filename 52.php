4b398049daa8b00dc6f7960526d26c52f1b9a048
Goldgram <willbrennandesign@gmail.com>
2013-07-29 12:18:39 +0200

    Finished, runs in 0.26s

--- 52.php
+++ 52.php
@@ -4,21 +4,43 @@ Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain
 -->
 <?php $startTime = microtime(true);
 
-for ($i=125870; $i <= 125875; $i++) { 
-	echo $i," => ",($i*2),"\n";
-	$checkStr = "".$i;
-	$multiple = ($i*2);
-	for ($j=0; $j < strlen($checkStr); $j++) { 
-		if () {
-			# code...
+function isPermutation($a,$b) {
+	$numsCountA = array();
+	for ($i=0; $i <= 9; $i++) { 
+		$numsCountA[$i] = substr_count($a,("".$i));
+	}
+	$numsCountB = array();
+	for ($i=0; $i <= 9; $i++) { 
+		$numsCountB[$i] = substr_count($b,("".$i));
+	}
+	if ($numsCountA == $numsCountB) {
+		return true;
+	}
+	return false;
+}
+function doesMultiplyUpToSix($input)
+{
+	for ($i=2; $i <= 6; $i++) { 
+		$checkStr = "".$input;
+		$multiple = ($input*$i);
+		if (!isPermutation($checkStr,$multiple)) {
+			return false;
 		}
 	}
-
+	return true;
+}
+$result=0;
+$i=125874;
+while ($result==0) {
+	if (doesMultiplyUpToSix($i)) {
+		$result=$i;
+		break;
+	}
+	$i++;
 }
 
-
-$answer = 0;
+$answer = $result;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
-// Answer: 
-// Time: 
+// Answer: 142857
+// Time: 0.26s
