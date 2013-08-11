45658ac84d165fd69cb626cfc99f3a761295abf7
Goldgram <willbrennandesign@gmail.com>
2013-08-11 16:20:03 +0100

    Complete, runs in 0.59s

--- 56.php
+++ 56.php
@@ -1,12 +1,25 @@
 <!-- 
-
+A googol (10100) is a massive number: one followed by one-hundred zeros; 100100 is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+Considering natural numbers of the form, ab, where a, b < 100, what is the maximum digital sum?
 -->
 <?php $startTime = microtime(true);
 
+$totalMax = 0;
+for ($i=1; $i < 100; $i++) {
+	for ($j=1; $j < 100; $j++) {
+		$numStr = bcpow("".$i,"".$j);
+		$total = 0;
+		for ($k=0; $k < strlen($numStr); $k++) {
+			$total += intval($numStr[$k]);
+		}
+		if ($total > $totalMax) {
+			$totalMax = $total;
+		}
+	}
+}
 
-
-$answer = 0;
+$answer = $totalMax;
 $endTime = microtime(true);
 echo "Answer: ",$answer,"\nTime: ",($endTime - $startTime),"\n";
-// Answer: 
-// Time: 
+// Answer: 972
+// Time: 0.59s
