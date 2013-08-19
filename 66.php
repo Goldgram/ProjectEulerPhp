7fbe72c038238b897036bed57f65cfb785a96545
Goldgram <willbrennandesign@gmail.com>
2013-08-19 23:10:29 +0100

    working on getting solution using convergence

--- 66.php
+++ 66.php
@@ -15,13 +15,17 @@ Find the value of D ≤ 1000 in minimal solutions of x for which the largest val
 <?php $startTime = microtime(true);
 
 $maxX = 0;
-for ($D=2; $D <= 1000; $D++) {
+
+for ($D=2; $D <= 16; $D++) {
+	// echo $D,"\n";
 	$currentD = sqrt($D);
 	if (floor($currentD) != $currentD) {
+	// 	echo $currentD,"\n";
 		for ($y=1; $y > 0; $y++) { 
 			$x = sqrt(($D*$y*$y)+1);
 			if (floor($x) == $x) {
-				echo $D," => ",$x,"\n";
+				// echo $D," => ",$x,"\n";
+				echo $D," => ",$y," >> ",$x,"\n";
 				if ($x>$maxX) {
 					$maxX = $x;
 				}
@@ -30,6 +34,40 @@ for ($D=2; $D <= 1000; $D++) {
 		}
 	}
 }
+
+// function numOfPeriods($input,$baseroot) {
+// 	// $count = 0;
+// 	// $m = 0;
+// 	// $d = 1;
+// 	$a = $baseroot-1;
+
+
+// 		$d = $input-($a*$a);
+// 		$a = floor(($a+$a)/$d);
+// 		echo ($a*floor(sqrt($input)))+1,"\n";
+	
+// }
+
+$startNum = 2;
+$endNum = 0;
+for ($baseRoot=2; $baseRoot <= 4; $baseRoot++) { 
+	$endNum = pow($baseRoot,2);
+	for ($D=$startNum; $D < $endNum; $D++) {
+		echo "=>",$D,"\n";
+		// numOfPeriods($D,$baseRoot);
+
+		$a = $baseRoot-1;
+		$d = $D-($a*$a);
+		$a = floor(($a+$a)/$d);
+		echo ($a*floor(sqrt($D)))+1,"\n";
+
+
+
+
+	}
+	$startNum = $endNum+1;
+}
+
 
 $answer = $maxX;
 $endTime = microtime(true);
