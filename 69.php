11fdfbb84ec06b9fdb22effd53e059717f7fe33b
Goldgram <willbrennandesign@gmail.com>
2013-09-10 07:47:33 +0100

    using multiplicative property to solve

--- 69.php
+++ 69.php
@@ -14,59 +14,78 @@ function isPrime($input) {
 	return true;
 }
 //set up array of primes up to needed
-$primeArray = array();
-$primeArray[0] = 2;
-// for ($num=3; $num < 1000; $num+=2) {
-$num=3;
-while (end($primeArray)<1000) {
-	if (isPrime($num))
-	{
-		$primeArray[] = $num;
-	}
-	$num+=2;
-}
+// $primeArray = array();
+// $primeArray[0] = 2;
+// // for ($num=3; $num < 1000; $num+=2) {
+// $num=3;
+// while (end($primeArray)<1000) {
+// 	if (isPrime($num))
+// 	{
+// 		$primeArray[] = $num;
+// 	}
+// 	$num+=2;
+// }
 
-function getTotient($input) {
-	global $primeArray;
-	$factorsArray = array();
-	$sq = sqrt($input);
-	for($i=0; $primeArray[$i] <= $sq; $i++) {
-		$primeFactor = $primeArray[$i];
-		if($input%$primeFactor==0) {
-			$factorsArray[] = $primeFactor;
-			// echo $primeFactor,",";
-			// $minusCount+=(($input/$primeFactor)-1);
+// function getTotient($input) {
+// 	global $primeArray;
+// 	$factorsArray = array();
+// 	$sq = sqrt($input);
+// 	for($i=0; $primeArray[$i] <= $sq; $i++) {
+// 		$primeFactor = $primeArray[$i];
+// 		if($input%$primeFactor==0) {
+// 			$factorsArray[] = $primeFactor;
+// 			// echo $primeFactor,",";
+// 			// $minusCount+=(($input/$primeFactor)-1);
 
-			$otherFactor = $input/$primeFactor;
-			if (isPrime($otherFactor) && $otherFactor%$primeFactor!=0) {
-				$factorsArray[] = $otherFactor;
-				// echo $otherFactor,",";
-				// $minusCount+=($primeFactor-1);
+// 			$otherFactor = $input/$primeFactor;
+// 			if (isPrime($otherFactor) && $otherFactor%$primeFactor!=0) {
+// 				$factorsArray[] = $otherFactor;
+// 				// echo $otherFactor,",";
+// 				// $minusCount+=($primeFactor-1);
+// 			}
+// 		}
+// 	}
+// 	$minusCount = 1;
+
+
+// // var_dump($factorsArray);
+// //calculate the real minus count...???
+
+
+// 	return " Tot:".($input - $minusCount);
+
+
+// }
+
+$primes = array();
+for ($num=2; $num <= 1000000; $num++) { 
+	if (isPrime($num)) {
+		$primes[] = $num;
+		// echo $num," -> ",($num-1),"\n";
+	} else {
+		$currentNum = $num;
+		$currentTotal = 1;
+		$primesCount = count($primes);
+		// for ($factor=2; $factor < $primesCount; $factor++) { 
+		foreach ($primes as $divisor) {
+			// $divisor = $key;
+			if ($divisor > $currentNum || $currentNum == 1) {
+				break;
+			}
+			if ($currentNum%$divisor==0) {
+				$currentTotal *= $divisor-1;
+				$currentNum = $currentNum/$divisor;
 			}
 		}
+		if ($currentNum > 1) {
+			$currentTotal *= $currentNum;
+		}
+		// echo $num," => ",$currentTotal,"\n";
+		// break;
 	}
-	$minusCount = 1;
-
-
-// var_dump($factorsArray);
-//calculate the real minus count...???
-
-
-	return " Tot:".($input - $minusCount);
-
-
+	// echo "============================>$num\n";
 }
-
-
-for ($num=2; $num <= 1000000; $num++) { 
-
-	echo $num," => ",getTotient($num),"\n";
-
-	// if ($num%1000==0) {
-	// 	echo $num,"\n";
-	// }
-}
-
+// var_dump($primes);
 
 $answer = 0;
 $endTime = microtime(true);
